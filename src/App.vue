ad80769f0490d8e50f614695250e9192fa81a159
xurmeinBackUps <wj.pavocrist@gmail.com>
2020-10-07 09:08:54 -0400

    initial; reset .git

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Welcome to the Forum</h1>
+    <div v-for="thread in threads" :key="thread.key">
+      <h2>{{ thread.title }}</h2>
+      <section v-if="thread.posts.count !== 0">
+        <div v-for="postId in thread.posts" :key="postId.key">
+          <p>"{{ posts[postId].text }}"
+            <label>&#8212; {{ users[posts[postId].userId].name }}</label>
+          </p>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import sampleData from '../data.json';
+console.log(sampleData)
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      threads: sampleData.threads,
+      posts: sampleData.posts,
+      users: sampleData.users
+    }
+  }
+}
+</script>
